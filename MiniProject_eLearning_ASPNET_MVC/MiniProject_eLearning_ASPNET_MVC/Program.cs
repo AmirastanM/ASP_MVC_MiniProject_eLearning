1fa8c8f49027bd61fa32c553f4f00e74d24ff47a
AmirastanM <amirastanshm@code.edu.az>
2024-06-05 12:55:11 +0400

    create foulder for miniProject

---
+++ MiniProject_eLearning_ASPNET_MVC/MiniProject_eLearning_ASPNET_MVC/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+
+
+
+
+
+
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
